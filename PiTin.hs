36d042ab47a1b1489c635841d645c569a01ea30b
Clarissa Littler <clarissa.littler@gmail.com>
2016-03-10 20:03:26 -0800

    stub file for translation

---
+++ PiTin.hs
@@ -0,0 +1,11 @@
+module PiTin where
+
+import qualified PiAST as P
+import qualified Tin as T
+
+stmtToProc :: T.Stmt -> P.Proc
+stmtToProc (Sexp e) = undefined
+stmtToProc (SReceive vs) = undefined
+stmtToProc (SSend Name es) = undefined
+stmtToProc (SWhile e s) = undefined
+
