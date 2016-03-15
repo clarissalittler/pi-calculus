f34b6bd2667920563cfa181b58536c4a1d50b7a8
Clarissa Littler <clarissa.littler@gmail.com>
2016-03-15 12:43:35 -0700

    added monad transformer for Fresher monad

--- Fresher.hs
+++ Fresher.hs
@@ -1,8 +1,10 @@
 module Fresher where
 
 import Control.Monad.State
+import Data.Functor.Identity
 
-type Fresher = State ([String],[String])
+type Fresher = FresherT Identity
+type FresherT m = StateT ([String],[String]) m
 
 infiniNames = zipWith (\x y -> [x] ++ (show y)) alphas [0..]
     where alphas = ['a'..'z'] ++ alphas
@@ -12,10 +14,16 @@ runFresher m ss = evalState m (ss,infiniNames)
 
 runFresher' m = runFresher m []
 
-nix :: String -> Fresher ()
+runFresherT :: Monad m => FresherT m a -> [String] -> m a
+runFresherT m ss = evalStateT m (ss,infiniNames)
+
+runFresherT' :: Monad m => FresherT m a -> m a
+runFresherT' m = runFresherT m []
+
+nix :: Monad m => String -> FresherT m ()
 nix s = modify (\(ex,inc) -> (s : ex,inc))
 
-fresh :: Fresher String
+fresh :: Monad m => FresherT m String
 fresh = do
   (excl,x : freshy) <- get
   if x `elem` excl 
